87b044620927134c2c021c74ffe8298183fd34f1
Serdar Gurbanmyradov <sgurbanmyradov@i-neti.ru>
2022-12-19 06:34:22 +0300

    fix bugs and catalog popup

--- nut-product/app/js/main.js
+++ nut-product/app/js/main.js
@@ -56,7 +56,6 @@ $(document).ready(function () {
 		// fullpage config - https://github.com/alvarotrigo/fullPage.js
 		let fp = {
 			containerSelector: '.fp-sections',
-			anchors: ['home', 'flour', 'mash', 'paste', 'praline', 'spices', 'nuts', 'chopped-nuts', 'inventory', 'marchpane'],
 		};
 
 		const flags = {
@@ -64,6 +63,7 @@ $(document).ready(function () {
 			mashFlag: true,
 			pasteFlag: true,
 			pralineFlag: true,
+			friedOnionFlag: true,
 			spicesFlag: true,
 			nutsFlag: true,
 			choppedNutsFlag: true,
@@ -74,7 +74,6 @@ $(document).ready(function () {
 		new fullpage(fp.containerSelector, {
 			menu: false,
 			lockAnchors: false,
-			anchors: fp.anchors,
 			lockAnchors: false,
 			navigation: false,
 			showActiveTooltip: false,
@@ -119,6 +118,12 @@ $(document).ready(function () {
 						if (flags.pralineFlag) {
 							pralineAnimation();
 							flags.pralineFlag = false;
+						}
+						break;
+					case 'fried-onion':
+						if (flags.friedOnionFlag) {
+							friedOnionAnimation();
+							flags.friedOnionFlag = false;
 						}
 						break;
 					case 'spices':
@@ -428,6 +433,52 @@ $(document).ready(function () {
 				gsap.to(item2, 2, { x: posX, y: posY });
 				gsap.to(item3, 2.5, { x: posX, y: posX });
 				gsap.to(item4, 2, { x: posX, y: posY });
+			})
+
+		}
+		setTimeout(() => {
+			mouseMoveParallax()
+			// infinite animation
+			gsap.fromTo(productsLeftImg, 2, { y: 0 }, { y: -25, repeat: -1, repeatDelay: 0, yoyo: true })
+			gsap.fromTo(productsRightImg, 2, { y: 0 }, { y: -25, repeat: -1, repeatDelay: 0, yoyo: true })
+		}, 2500);
+	}
+	
+	function friedOnionAnimation() {
+		const productsLeftImg = document.querySelector('.products-presentation--fried-onion .products-presentation__img-wrap--left')
+		const productsRightImg = document.querySelector('.products-presentation--fried-onion .products-presentation__img-wrap--right')
+		const item1 = document.querySelector('.products-presentation--fried-onion .products-presentation__item:nth-child(1)')
+		const item2 = document.querySelector('.products-presentation--fried-onion .products-presentation__item:nth-child(2)')
+		const item3 = document.querySelector('.products-presentation--fried-onion .products-presentation__item:nth-child(3)')
+		const item4 = document.querySelector('.products-presentation--fried-onion .products-presentation__item:nth-child(4)')
+		const item5 = document.querySelector('.products-presentation--fried-onion .products-presentation__item:nth-child(5)')
+		const item6 = document.querySelector('.products-presentation--fried-onion .products-presentation__item:nth-child(6)')
+		const item7 = document.querySelector('.products-presentation--fried-onion .products-presentation__item:nth-child(7)')
+
+		// one-off animation
+		gsap.fromTo(productsRightImg, 1, { y: 150, opacity: 0 }, { y: 0, opacity: 1}).delay(1)
+		gsap.fromTo(productsLeftImg, 1, { y: 150, opacity: 0 }, { y: 0, opacity: 1}).delay(0.8)
+		gsap.fromTo(item1, 0.5, { y: 30, scale: 0 }, { y:0, scale: 1}).delay(1.2)
+		gsap.fromTo(item2, 0.5, { y: 40, scale: 0 }, { y:0, scale: 1}).delay(1.3)
+		gsap.fromTo(item3, 0.5, { y: 50, scale: 0 }, { y:0, scale: 1}).delay(1.4)
+		gsap.fromTo(item4, 0.5, { y: 50, scale: 0 }, { y:0, scale: 1}).delay(1.6)
+		gsap.fromTo(item5, 0.5, { y: 50, scale: 0 }, { y:0, scale: 1}).delay(1.7)
+		gsap.fromTo(item6, 0.5, { y: 40, scale: 0 }, { y:0, scale: 1}).delay(1.8)
+		gsap.fromTo(item7, 0.5, { y: 30, scale: 0 }, { y:0, scale: 1}).delay(1.9)
+
+		function mouseMoveParallax() {
+			$('.category-section--fried-onion').on('mousemove', function (e) {
+				const posX = e.clientX / 100;
+				const posY = e.clientY / 100;
+				
+				// items animation
+				gsap.to(item1, 1, { x: posX, y: posY });
+				gsap.to(item2, 2, { x: posX, y: posY });
+				gsap.to(item3, 2.5, { x: posX, y: posX });
+				gsap.to(item4, 2, { x: -posX, y: -posY });
+				gsap.to(item5, 2, { x: posX, y: posY });
+				gsap.to(item6, 2, { x: -posX, y: -posY });
+				gsap.to(item7, 2, { x: -posX, y: -posY });
 			})
 
 		}
